f43d14310abe18c752b420565cbde3ceb817e722
Tino Kallinich-Friedrich <tino.kallinich-friedrich@inovex.de>
2025-03-24 14:04:11 +0100

    REF 52: moved request call from service to api

--- app/src/main/java/de/showcase/tezos_viewer/domains/blocks/BlocksService.kt
+++ app/src/main/java/de/showcase/tezos_viewer/domains/blocks/BlocksService.kt
@@ -2,28 +2,14 @@ package de.showcase.tezos_viewer.domains.blocks
 
 import Block
 import de.showcase.tezos_viewer.domains.shared.Api
-import io.ktor.client.call.body
-import io.ktor.client.request.get
+import de.showcase.tezos_viewer.domains.shared.Request
 import io.ktor.http.Url
-import io.ktor.http.headers
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
-import kotlinx.coroutines.withTimeout
-import timber.log.Timber
-import java.util.concurrent.TimeoutException
-
-data class Request(
-    val url: Url,
-    val apiKey: String,
-)
 
 class BlocksService(private val api: Api) {
 
-    fun checkForProAccess(): Boolean {
-        return api.environment.apiKey.isNotEmpty()
-    }
+    fun checkForProAccess(): Boolean = api.environment.apiKey.isNotEmpty()
 
     suspend fun getBlocks(): List<Block>? {
         val request = Request(
@@ -31,40 +17,20 @@ class BlocksService(private val api: Api) {
             apiKey = api.environment.apiKey
         )
 
-        return try {
-            withContext(Dispatchers.Default.limitedParallelism(1)) {
-                withTimeout(api.timeout) {
-                    api.ktorClient.get(request.url) {
-                        headers {
-                            if (checkForProAccess()) {
-                                append("Authorization", "Bearer ${request.apiKey}")
-                            }
-                        }
-                    }
-                }
-            }.let { response ->
-                Timber.d("getBlocks():\nrequest=$request\nresponse=$response")
+        val response = withContext(Dispatchers.Default.limitedParallelism(1)) {
+            api.GET<List<Block>>(request)
+        }
 
-                when (response.status.value) {
-                    200 -> {
-                        return response.body<List<Block>>()
-                    }
-
-                    429 -> {
-                        throw Exception("${response.status} Rate limit exceeded!")
-                    }
-
-                    else -> {
-                        throw Exception("${response.status} Could not fetch blocks!")
-                    }
-                }
+        return when (response.statusCode) {
+            200 -> {
+                response.body
             }
-        } catch (e: TimeoutCancellationException) {
-            Timber.e("getBlocks(): Timeout Error $e")
-            null
-        } catch (e: Exception) {
-            Timber.e("getBlocks(): Error $e")
-            emptyList()
+            429 -> {
+                null // Timeout
+            }
+            else -> {
+                emptyList()
+            }
         }
     }
 }
--- app/src/main/java/de/showcase/tezos_viewer/domains/shared/Api.kt
+++ app/src/main/java/de/showcase/tezos_viewer/domains/shared/Api.kt
@@ -1,20 +1,67 @@
+@file:Suppress("FunctionName")
+
 package de.showcase.tezos_viewer.domains.shared
 
 import de.showcase.tezos_viewer.environment.Environment
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.http.Url
+import io.ktor.http.headers
 import io.ktor.serialization.kotlinx.json.json
+import io.ktor.util.StringValues
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+
+data class Response<T>(
+    val statusCode: Int,
+    val headers: StringValues,
+    val body: T?,
+)
+
+data class Request(
+    val url: Url,
+    val apiKey: String,
+)
 
 class Api(
-    val timeout: Long = 5000,
+    val timeout: Long = 4000,
     val environment: Environment,
     private val engine: HttpClientEngine = CIO.create(),
 ) {
     val ktorClient = HttpClient(engine) {
         install(ContentNegotiation) {
             json()
+        }
+    }
+
+    suspend inline fun <reified T> GET(request: Request): Response<T> {
+        val requestHeaders = headers {
+            if (environment.apiKey.isNotEmpty()) {
+                append("Authorization", "Bearer ${request.apiKey}")
+            }
+        }
+
+        return try {
+            withTimeout(timeout) {
+                val httpResponse = ktorClient.get(request.url) {
+                    headers.appendAll(requestHeaders)
+                }
+                Response(
+                    statusCode = httpResponse.status.value,
+                    headers = httpResponse.headers,
+                    body = httpResponse.body<T>(),
+                )
+            }
+        } catch (e: TimeoutCancellationException) {
+            Response(
+                statusCode = 429,
+                headers = requestHeaders,
+                body = null,
+            )
         }
     }
 }
